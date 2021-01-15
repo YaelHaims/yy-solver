561b68d4b0ee6ba52a332697e8c190ac2dab8c18
Yuval <yuvalmich2@gmail.com>
2021-01-15 12:06:00 +0200

    calculate while true

--- src/server_side/MyClientHandler.java
+++ src/server_side/MyClientHandler.java
@@ -20,48 +20,51 @@ public class MyClientHandler implements ClientHandler {
 		BufferedReader in = new BufferedReader(new InputStreamReader(input));
 		PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(output)), true);
 		try {
-			System.out.println("start reading matrix weights");
-			List<List<Double>> lines = new ArrayList<List<Double>>();
-			String line = in.readLine();
-			
-			while(!line.equals("end")) {
-				lines.add(Arrays.asList(line.split(","))
-					 .stream()
-					 .map(s -> Double.parseDouble(s))
-					 .collect(Collectors.toList()));
-				line = in.readLine();
+			while(true) {
+				System.out.println("start reading matrix weights");
+				List<List<Double>> lines = new ArrayList<List<Double>>();
+				String line = in.readLine();
+				
+				while(!line.equals("end")) {
+					lines.add(Arrays.asList(line.split(","))
+						 .stream()
+						 .map(s -> Double.parseDouble(s))
+						 .collect(Collectors.toList()));
+					line = in.readLine();
+				}
+				
+				System.out.println("finish reading matrix weights, first weight is: " + lines.get(0).get(0));
+				
+				String s = in.readLine();
+				System.out.println("entry location: " + s);
+				
+				String[] entryLocation = s.split(",");
+				int entryRow = Integer.parseInt(entryLocation[1]);
+				int entryCol = Integer.parseInt(entryLocation[0]);
+				BoardCell entry = new BoardCell(entryRow, entryCol, lines.get(entryRow).get(entryCol));
+				
+				s = in.readLine();
+				System.out.println("exit location: " + s);
+				
+				String[] exitLocation = s.split(",");
+				int exitRow = Integer.parseInt(exitLocation[1]);
+				int exitCol = Integer.parseInt(exitLocation[0]);
+				BoardCell exit = new BoardCell(exitRow, exitCol, lines.get(exitRow).get(exitCol));
+				
+				System.out.println("creating board");
+				SearchableBoard board = new SearchableBoard(lines, entry, exit);
+				System.out.println("board created");
+				
+				BestFirstSearcher searcher = new BestFirstSearcher();
+				
+				System.out.println("start searching for best path");
+				Solution solution = searcher.search(board);
+				System.out.println("found best path");
+				System.out.println(solution.toString());
+				out.println(solution.toString());
+
 			}
-			
-			System.out.println("finish reading matrix weights, first weight is: " + lines.get(0).get(0));
-			
-			String s = in.readLine();
-			System.out.println("entry location: " + s);
-			
-			String[] entryLocation = s.split(",");
-			int entryRow = Integer.parseInt(entryLocation[0]);
-			int entryCol = Integer.parseInt(entryLocation[1]);
-			BoardCell entry = new BoardCell(entryRow, entryCol, lines.get(entryRow).get(entryCol));
-			
-			s = in.readLine();
-			System.out.println("exit location: " + s);
-			
-			String[] exitLocation = s.split(",");
-			int exitRow = Integer.parseInt(exitLocation[0]);
-			int exitCol = Integer.parseInt(exitLocation[1]);
-			BoardCell exit = new BoardCell(exitRow, exitCol, lines.get(exitRow).get(exitCol));
-			
-			System.out.println("creating board");
-			SearchableBoard board = new SearchableBoard(lines, entry, exit);
-			System.out.println("board created");
-			
-			BestFirstSearcher searcher = new BestFirstSearcher();
-			
-			System.out.println("start searching for best path");
-			Solution solution = searcher.search(board);
-			System.out.println("found best path");
-			System.out.println(solution.toString());
-			out.println(solution.toString());
-			
+						
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {

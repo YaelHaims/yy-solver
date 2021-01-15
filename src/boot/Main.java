87e9d94804563e9e0e57c43714ba52f70ededb78
Yuval <yuvalmich2@gmail.com>
2021-01-15 11:53:47 +0200

    use my client handler in main

--- src/boot/Main.java
+++ src/boot/Main.java
@@ -2,6 +2,7 @@ package boot;
 
 import server_side.CacheManager;
 import server_side.FileCacheManager;
+import server_side.MyClientHandler;
 import server_side.MySerialServer;
 import server_side.MyTestClientHandler;
 import server_side.StringReverser;
@@ -11,8 +12,7 @@ public class Main {
 	public static void main(String[] args) {
 		MySerialServer server = new MySerialServer();
 		int port = 9000;
-		CacheManager<String, String> cacheManager = new FileCacheManager<String, String>();
-		MyTestClientHandler c = new MyTestClientHandler(new StringReverser(), cacheManager);
+		MyClientHandler c = new MyClientHandler();
 		server.open(port, c);
 	}
 }
